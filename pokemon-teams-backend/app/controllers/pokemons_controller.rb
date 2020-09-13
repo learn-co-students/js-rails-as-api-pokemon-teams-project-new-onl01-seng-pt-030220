2feb42bd760cc2f669b908b7df328b2565b05b97
Rkebede <rutht.kebede@gmail.com>
2020-09-12 22:38:43 -0500

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,23 @@
+class PokemonsController < ApplicationController
+  
+  def index
+    pokemons = Pokemon.all
+    render json: pokemons, only: [:id, :nickname, :species, :trainer_id]
+  end
+
+  def show
+    pokemon = Pokemon.find(params[:id])
+    render json: pokemon, only: [:id, :nickname, :species, :trainer_id]
+  end
+
+  def create
+    trainer = Trainer.find(params[:trainer_id])
+    name = Faker::Name.first_name
+    species = Faker::Games::Pokemon.name
+    render json: Pokemon.create(nickname: name, species: species, trainer_id: trainer.id)
+  end
+
+  def destroy
+    Pokemon.find(params[:id]).destroy
+  end
+end

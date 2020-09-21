5049abaa4ff3abcb7b33f8b98bc9f470c8ae5f43
Haloren <mckennakyler@gmail.com>
2020-09-21 14:59:21 -0700

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,19 @@
 class PokemonsController < ApplicationController
+    def index
+        @pokemons =Pokemon.all
+        render json: @pokemons
+    end
+
+    def create 
+        #byebug
+
+    end
+
+    def destroy
+        pokemon = Pokemon.all.find_by(id: params[:id])
+        trainer = pokemon.trainer
+        pokemon.destroy
+        render json: trainer, include: [:pokemons]
+    end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,35 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetchTrainers();
+})
+
+//<main> in index contains the cards
+const main = document.querySelector('#main'); 
+
+// FETCH TRAINER DATA
+function fetchTrainers() {
+    fetch(TRAINERS_URL)
+    .then(res => res.json())
+    .then(json => console.log(json)); 
+}    
+
+// CREATE TRAINER CARDS 
+function addCard(obj) {
+    // <div class="card" data-id="1"><p>Prince</p>
+    let div = document.createElement('div');
+    div.class = "card";
+    div.setAttribute('data-id', `${obj.id}`);
+
+    let p = document.createElement('p');
+    p.innerHTML = `${obj.name}`;
+    div.append(p);
+    
+    //<button data-trainer-id="1">Add Pokemon</button>
+    let addButton = document.createElement('button');
+    addButton.innerText = 'Add Pokemon';
+    div.appendChild(addButton);
+    
+}

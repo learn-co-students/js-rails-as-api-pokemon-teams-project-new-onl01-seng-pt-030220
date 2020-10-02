0d2aef3a6df114a547dfdb68b0ba9e6e1c2bbd17
Haloren <mckennakyler@gmail.com>
2020-10-01 19:57:05 -0700

    finished

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,12 +1,38 @@
 class PokemonsController < ApplicationController
     def index
-        @pokemons =Pokemon.all
-        render json: @pokemons
+        pokemons =Pokemon.all
+        render json: pokemons
+    end
+
+    def show
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: pokemon
     end
 
     def create 
         #byebug
-
+        trainer = Trainer.find_by(id: params[:trainer_id])
+        pokemon = trainer.pokemons.build({
+            nickname: Faker::Name.first_name,
+            species: Faker::Games::Pokemon.name
+        })  
+        if pokemon.save 
+            # byebug 
+            render json: pokemon
+        else 
+            # byebug
+            render json: {message: pokemon.errors.messages[:max][0]}
+        end
+        # if trainer.pokemons.length < 6
+        #     name = Faker::Name.first_name
+        #     species = Faker::Games::Pokemon.name
+        #     pokemon = Pokemon.create(
+        #         nickname: name, 
+        #         species: species, 
+        #         trainer_id: trainer.id
+        #     )
+        #     render json: pokemon
+        # end
     end
 
     def destroy
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,34 +2,87 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-document.addEventListener("DOMContentLoaded", function(){
-    fetchTrainers();
-})
+const main = document.querySelector("main")
 
-//<main> in index contains the cards
-const main = document.querySelector('#main'); 
+document.addEventListener("DOMContentLoaded", () => loadTrainers())
 
-// FETCH TRAINER DATA
-function fetchTrainers() {
+const loadTrainers = () => {
     fetch(TRAINERS_URL)
-    .then(res => res.json())
-    .then(json => console.log(json)); 
-}    
+    .then(resp => resp.json())
+    // .then(json => console.log(json))
+    .then(json => {
+        json.forEach(trainer => renderTrainer(trainer))
+    })
+}
 
-// CREATE TRAINER CARDS 
-function addCard(obj) {
-    // <div class="card" data-id="1"><p>Prince</p>
-    let div = document.createElement('div');
-    div.class = "card";
-    div.setAttribute('data-id', `${obj.id}`);
+const renderTrainer = (trainerHash) => {
+    const div = document.createElement("div")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    const ul = document.createElement("ul")
 
-    let p = document.createElement('p');
-    p.innerHTML = `${obj.name}`;
-    div.append(p);
+    div.setAttribute("class", "card")
+    div.setAttribute("data-id", trainerHash.id)
     
-    //<button data-trainer-id="1">Add Pokemon</button>
-    let addButton = document.createElement('button');
-    addButton.innerText = 'Add Pokemon';
-    div.appendChild(addButton);
+    p.innerHTML = trainerHash.name
     
+    button.setAttribute("data-trainer-id", trainerHash.id)
+    button.innerHTML = "Add Pokemon"
+    //eventListener for button on click
+    button.addEventListener("click", createPokemon)
+
+    div.appendChild(p)
+    div.appendChild(button)
+    div.appendChild(ul)
+
+    main.appendChild(div)
+    trainerHash.pokemons.forEach(pokemon => renderPokemon(pokemon))
+}
+const renderPokemon = (pokemon) => {
+    const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+
+    li.innerHTML = `${pokemon.nickname} - ${pokemon.species}`
+    button.setAttribute("class", "release")
+    button.setAttribute("data-pokemon-id", pokemon.id)
+    button.innerHTML = "Release"
+    //eventListner for button on click
+    button.addEventListener("click", deletePokemon)
+
+    li.appendChild(button)
+    ul.appendChild(li)
+}
+// function createPokemon()
+const createPokemon = (e) => {
+    e.preventDefault()
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({trainer_id: e.target.dataset.trainerId})
+    }
+
+    fetch(POKEMONS_URL, configObj)
+    .then(resp => resp.json())
+    .then(json => 
+        json.message ? alert(json.message) : renderPokemon(json) 
+    )
+}
+//function deletePokemon()
+const deletePokemon = (e) => {
+    e.preventDefault()
+    const configObj = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        //nothing to do here because nothing is being created
+    }
+
+    fetch(`${POKEMONS_URL}/${e.target.dataset.pokemonId}`, configObj)
+        e.target.parentElement.remove()
 }

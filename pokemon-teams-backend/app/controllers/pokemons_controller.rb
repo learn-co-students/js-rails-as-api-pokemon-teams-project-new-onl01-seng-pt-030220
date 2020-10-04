24312e8dca561b550c250bf2a8c41e401a78969d
antimonysayz <antimonysayz@gmail.com>
2020-10-04 00:34:12 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -0,0 +1,36 @@
+class PokemonsController < ApplicationController
+
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons
+    end
+
+
+    def show
+        pokemon = Pokemon.find_by(id: params[:id])
+        render json: pokemon
+    end
+
+    def create 
+       
+        trainer = Trainer.find_by(id: params[:trainer_id])
+        pokemon = trainer.pokemons.build({
+            nickname: Faker::Name.first_name,
+            species: Faker::Games::Pokemon.name
+        })  
+        if pokemon.save 
+            render json: pokemon
+        else 
+            render json: {message: pokemon.errors}
+        end
+    
+    end
+
+    def destroy
+        pokemon = Pokemon.all.find_by(id: params[:id])
+        trainer = pokemon.trainer
+        pokemon.destroy
+        render json: trainer, include: [:pokemons]
+    end
+
+end
